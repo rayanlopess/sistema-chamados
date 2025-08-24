6d55393aa13dc324b026cd6031919ccbf0119637
Your Name <you@example.com>
2025-08-24 00:09:11 -0300

    1.0

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home'; // ← Importe a classe Home
+import { Dashboard } from './dashboard/dashboard'; // ← Importe a classe Home
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home }, // ← Use Home (não HomeComponent)
+  { path: 'dashboard', component: Dashboard }
+];
---
+++ src/app/home/home.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
+
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.scss'
+})
+export class Home implements OnInit{
+  ngOnInit(): void {
+    
+  }
+
+}
